fb6e7d02513ae71a64a4d1e19f1be0a460d274d6
Jose Villalba <jjvillalba97@gmail.com>
2022-07-22 15:13:34 -0400

    change latex.js to unified.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-import { parse, HtmlGenerator } from "latex.js";
-import { createHTMLWindow } from "svgdom";
 import * as fs_extra from "fs-extra";
 import * as fs from "fs";
-import { JSDOM } from "jsdom";
+import { unifiedLatexFromString } from "@unified-latex/unified-latex-util-parse";
 import { execSync } from "child_process";
+import { JSDOM } from "jsdom";
+import { macros } from "./src/custom_macros.js";
+import { unifiedLatexToHast } from "@unified-latex/unified-latex-to-hast";
+import { unified } from "unified";
+import rehypeStringify from "rehype-stringify/lib/index.js";
+import katex from "katex";
 import { format } from "prettier";
 
 const MAKE_PDF = true;
@@ -13,95 +17,109 @@ const MAKE_PDF = true;
  */
 
 if (MAKE_PDF) {
-  await fs_extra.copy("./book", "./tmp", {});
+  try {
+    await fs_extra.copy("./book", "./tmp", {});
 
-  execSync("cd ./tmp && pdflatex main.tex");
+    execSync("cd ./tmp && pdflatex main.tex");
 
-  fs.copyFileSync("./tmp/main.pdf", "./output/main.pdf");
+    fs.copyFileSync("./tmp/main.pdf", "./output/main.pdf");
+  } catch (error) {
+    console.log(error.output.toString());
+    process.exit(0);
+  }
 }
 
 /**
  * COMPILE HTML
  */
 
-const latex = fs.readFileSync("./book/main.tex", { encoding: "utf-8" });
+// global.window = createHTMLWindow();
+// global.document = window.document;
 
-global.window = createHTMLWindow();
-global.document = window.document;
+const _main_file = fs.readFileSync("./book/main.tex", { encoding: "utf-8" });
 
-const includeRegEx = /\\include{(.*?)}/g;
-const inputRegEx = /\\\\input{(.*?)}/g;
-const expanded = latex
+const latex_doc = _main_file
+  // Remove preamble
   .replace("\\input{preamble.tex}", "")
-  // Replace Include
-  .replaceAll(includeRegEx, (match) => {
+  // Expand include macro
+  .replaceAll(/\\include{(.*?)}/g, (match) => {
     const path = match.replace("\\include{", "./book/").replace("}", "");
     return fs.readFileSync(path, { encoding: "utf-8" });
   })
-  // Replace Input
-  .replaceAll(inputRegEx, (match) => {
+  // Expand input macro
+  .replaceAll(/\\\\input{(.*?)}/g, (match) => {
     const path = match.replace("\\input{", "./book/").replace("}", "");
     return fs.readFileSync(path, { encoding: "utf-8" });
+  });
+
+// const latex_doc = macro_substitution(_expanded_latex);
+
+const latex_html = unified()
+  .use(unifiedLatexFromString, {
+    macros: {
+      label: { signature: "m" },
+      HTMLclassTitle: { signature: "m" },
+    },
   })
-  // Some simple macro replacement
-  .replace("\\Q", "\\mathbb{Q}");
+  .use(unifiedLatexToHast)
+  .use(rehypeStringify)
+  .processSync(latex_doc);
 
-let generator = new HtmlGenerator({
-  hyphenate: true,
-  CustomMacros: (function () {
-    let args = (CustomMacros.args = {});
-    let prototype = CustomMacros.prototype;
+// console.log(latex_html)
 
-    function CustomMacros(generator) {
-      this.generator = generator;
-    }
-
-    args["htmlDiv"] = ["V", "i"];
-    prototype["htmlDiv"] = function (className) {
-      let divElement = this.generator.create("div");
-      divElement.className = className;
-      return [divElement];
-    };
-
-    // prototype["R"] = function () {
-    // return "";
-    // };
-
-    return CustomMacros;
-  })(),
+const jsdom = new JSDOM(latex_html.value);
+jsdom.window.document.querySelectorAll(".display-math").forEach((node) => {
+  node.innerHTML = katex.renderToString(node.innerHTML, {
+    displayMode: true,
+    macros: macros,
+    fleqn: true,
+  });
 });
 
-let doc = parse(expanded, { generator: generator }).htmlDocument();
-
-const dom = new JSDOM(doc.documentElement.outerHTML.replace("</meta>", ""));
-
-const pages = dom.window.document.querySelectorAll(".newPage");
-
-pages.forEach((value, index) => {
-  fs.writeFileSync(
-    `./output/pages/page-${index}.html`,
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page ${index}</title>
-    <link type="text/css" rel="stylesheet" href="../css/katex.css" />
-    <link type="text/css" rel="stylesheet" href="../css/book.css" />
-    <script src="../js/base.js"></script>
-</head>
-<body>
-    ${value.innerHTML}
-</body>
-</html>
-`
-  );
+jsdom.window.document.querySelectorAll(".inline-math").forEach((node) => {
+  node.innerHTML = katex.renderToString(node.innerHTML, { macros: macros });
 });
 
-const html = dom.window.document.querySelector("html").outerHTML;
+jsdom.window.document.querySelectorAll(".macro-label").forEach((node) => {
+  node.id = node.innerHTML.replace("{", "").replace("}", "");
+});
 
-const prettyHtml = format(html, { parser: "html" });
+jsdom.window.document.querySelectorAll(".macro-ref").forEach((node) => {
+  const refId = node.innerHTML.replace("{", "").replace("}", "");
+  node.outerHTML = `<a href="#${refId}">ref</a>`;
+});
 
-fs.writeFileSync("./output/index.html", prettyHtml);
+jsdom.window.document.querySelectorAll(".macro-HTMLclassTitle").forEach((node) => {
+  node.innerHTML = node.innerHTML.replace("{", "").replace("}", "");
+});
+
+const html = `
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link type="text/css" rel="stylesheet" href="output/css/base.css">
+     <link type="text/css" rel="stylesheet" href="output/katex/katex.min.css">
+     <title>Notas</title>
+ </head>
+ <body>
+    <header>
+      <div class="title">
+        <h1>Matemáticas</h1>
+        <h3>Notas para una licenciatura</h3>
+      </div>
+      <h2>José Julián Villalba Vásquez</h2>
+    </header>
+    <main>
+        ${format(
+          jsdom.window.document.documentElement.querySelector("body").innerHTML,
+          { parser: "html" }
+        )}
+      </main>
+    </body>
+ </html>
+ `;
+
+fs.writeFileSync("./index.html", html);
---
+++ src/custom_macros.js
@@ -0,0 +1,6 @@
+export const macros = {
+  "\\R": "\\mathbb{R}",
+  "\\Q": "\\mathbb{Q}",
+  "\\F": "\\mathbb{F}",
+  "\\C": "\\mathbb{C}",
+};
